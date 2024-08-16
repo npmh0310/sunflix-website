fc2783db42fec3e8d45785667117db506ca351d3
minhhieu310 <nguyenphuocminhhieu310@gmail.com>
2024-08-16 15:15:30 +0700

    homepage vs media detail

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import HeroSlide from "../components/common/HeroSlide";
+import tmdbConfigs from "../api/configs/tmdb.config.js";
+import uiConfigs from "../configs/ui.configs.js";
+import { Box } from "@mui/material";
+import MediaSlide from "./../components/common/MediaSlide";
+import Container from "../components/common/Container";
 
 const HomePage = () => {
-  return <div></div>;
+  return (
+    <>
+      <HeroSlide
+        mediaType={tmdbConfigs.mediaType.movie}
+        mediaCategory={tmdbConfigs.mediaCategory.popular}
+      />
+
+      <Box marginTop="-4rem" sx={{ ...uiConfigs.style.mainContent }}>
+        <Container header="popular movies">
+          <MediaSlide
+            mediaType={tmdbConfigs.mediaType.movie}
+            mediaCategory={tmdbConfigs.mediaCategory.popular}
+          />
+        </Container>
+
+        <Container header="popular series">
+          <MediaSlide
+            mediaType={tmdbConfigs.mediaType.tv}
+            mediaCategory={tmdbConfigs.mediaCategory.popular}
+          />
+        </Container>
+
+        <Container header="top rated movies">
+          <MediaSlide
+            mediaType={tmdbConfigs.mediaType.movie}
+            mediaCategory={tmdbConfigs.mediaCategory.top_rated}
+          />
+        </Container>
+
+        <Container header="top rated series">
+          <MediaSlide
+            mediaType={tmdbConfigs.mediaType.tv}
+            mediaCategory={tmdbConfigs.mediaCategory.top_rated}
+          />
+        </Container>
+      </Box>
+    </>
+  );
 };
 
 export default HomePage;
